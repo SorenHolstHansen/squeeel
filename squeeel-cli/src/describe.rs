2bd5bac44861c41287ae068b05fd956389735bdc
SorenHolstHansen <shan@wayfare.ai>
2025-06-18 10:07:34 +0200

    chore: cleanup

--- squeeel-cli/src/describe.rs
+++ squeeel-cli/src/describe.rs
@@ -15,18 +15,10 @@ pub async fn init_pg_pool(database_url: &str) -> anyhow::Result<()> {
     Ok(())
 }
 
-pub fn pg_pool() -> &'static PgPool {
-    PG_POOL.get().unwrap()
-}
-
 static SQLITE_POOL: OnceCell<SqlitePool> = OnceCell::const_new();
 pub async fn init_sqlite_pool(database_url: &str) -> anyhow::Result<()> {
     SQLITE_POOL.set(SqlitePool::connect(database_url).await?)?;
     Ok(())
-}
-
-pub fn sqlite_pool() -> &'static SqlitePool {
-    SQLITE_POOL.get().unwrap()
 }
 
 static MY_SQL_POOL: OnceCell<MySqlPool> = OnceCell::const_new();
@@ -35,15 +27,11 @@ pub async fn init_my_sql_pool(database_url: &str) -> anyhow::Result<()> {
     Ok(())
 }
 
-pub fn my_sql_pool() -> &'static MySqlPool {
-    MY_SQL_POOL.get().unwrap()
-}
-
 impl DbExt for Postgres {
     type Db = Postgres;
 
     async fn describe(query: String) -> Result<Describe<Self::Db>, sqlx::Error> {
-        pg_pool().describe(&query).await
+        PG_POOL.get().unwrap().describe(&query).await
     }
 }
 
@@ -51,7 +39,7 @@ impl DbExt for Sqlite {
     type Db = Sqlite;
 
     async fn describe(query: String) -> Result<Describe<Self::Db>, sqlx::Error> {
-        sqlite_pool().describe(&query).await
+        SQLITE_POOL.get().unwrap().describe(&query).await
     }
 }
 
@@ -59,6 +47,6 @@ impl DbExt for MySql {
     type Db = MySql;
 
     async fn describe(query: String) -> Result<Describe<Self::Db>, sqlx::Error> {
-        my_sql_pool().describe(&query).await
+        MY_SQL_POOL.get().unwrap().describe(&query).await
     }
 }
