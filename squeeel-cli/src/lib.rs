aef0229ea2b97056247b9de75b281268758b5011
SorenHolstHansen <shan@wayfare.ai>
2025-06-20 14:36:28 +0200

    better separation of concerns

--- squeeel-cli/src/lib.rs
+++ squeeel-cli/src/lib.rs
@@ -1,7 +1,7 @@
 mod sql_libs;
 pub use sql_libs::*;
 mod visitor;
-pub use visitor::{AstVisitor, Query};
+pub use visitor::{Query, visit_ast};
 mod describe;
 pub use describe::{init_my_sql_pool, init_pg_pool, init_sqlite_pool};
 mod utils;
--- squeeel-cli/src/visitor.rs
+++ squeeel-cli/src/visitor.rs
@@ -2,6 +2,7 @@ use crate::SupportedLib;
 use swc_core::ecma::visit::Visit;
 use swc_core::ecma::visit::VisitWith as _;
 use swc_ecma_ast::CallExpr;
+use swc_ecma_ast::Module;
 
 #[derive(Debug, Clone)]
 pub struct Query {
@@ -9,7 +10,7 @@ pub struct Query {
     pub lib: SupportedLib,
 }
 
-pub struct AstVisitor<'a> {
+struct AstVisitor<'a> {
     libs: &'a [SupportedLib],
     statements: Vec<Query>,
 }
@@ -19,6 +20,7 @@ impl Visit for AstVisitor<'_> {
         for lib in self.libs {
             if let Some(statement) = lib.parse_call_expr(call_expr) {
                 self.statements.push(statement);
+                // TODO: allow for more than one lib to detect queries here, but perhaps add a warning
                 return;
             }
         }
@@ -33,8 +35,10 @@ impl<'a> AstVisitor<'a> {
             statements: Vec::new(),
         }
     }
+}
 
-    pub fn statements(self) -> Vec<Query> {
-        self.statements
-    }
+pub fn visit_ast(supported_libs: &[SupportedLib], module: &Module) -> Vec<Query> {
+    let mut ast_visitor = AstVisitor::new(supported_libs);
+    ast_visitor.visit_module(module);
+    ast_visitor.statements
 }
